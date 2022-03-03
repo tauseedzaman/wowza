e31dd71005a304e894a4f71599fcf58f4ebac511
tauseedzaman <tauseedzamanofsorana@gmail.com>
2022-03-03 09:04:25 +0500

    kernel sheduler updated

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Http;
 
 class Kernel extends ConsoleKernel
 {
-    public $input_data;
+    public $stream;
     /**
      * Define the application's command schedule.
      *
@@ -18,36 +18,31 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $this->input_data = streamTargetSchedule::latest()->first();
 
-        $schedule->call(function () {
-            echo "this is at ;)";
-        })->when(false);
+        if (streamTargetSchedule::count()) {
+            $streams = streamTargetSchedule::all();
 
-        if ($this->input_data) {
-            $schedule->call(function () {
-                echo "starting success....\n\n";
-                // send api request to wowza for starting stream
-                if (streamTargetSchedule::count()) {
-                    $stream = streamTargetSchedule::latest()->first();
-                    $this->enable_this_stream($stream->app, $stream->stream);
-                }
-            })->when(function () {
-                return \Carbon\Carbon::createFromFormat('m/d/Y H:i', $this->input_data->start_time->format('m/d/Y H:i'))->isPast();
-            });
-
-
-
-
-            if (\Carbon\Carbon::createFromFormat('m/d/Y H:i', $this->input_data->start_time->format('m/d/Y H:i'))->isPast() && \Carbon\Carbon::createFromFormat('m/d/Y H:i', $this->input_data->end_time->format('m/d/Y H:i'))->isPast()) {
-                echo "Stoped success....\n\n";
-                // delete schedule from the database
-                $stream = streamTargetSchedule::latest()->first();
-                $this->disable_stream_target($stream->app, $stream->stream);
-                $this->input_data->delete();
+            // loop thro all schedule stream targets and starts them if there time is to start
+            foreach ($streams as $this->stream) {
+                $schedule->call(function () {
+                    $this->enable_this_stream($this->stream->app, $this->stream->stream);
+                    // echo "starting success....\n\n";
+                })->when(function () {
+                    return \Carbon\Carbon::createFromFormat('m/d/Y H:i', $this->stream->start_time->format('m/d/Y H:i'))->isPast();
+                });
             }
 
 
+            // loop thro all schdule stream targets and disable and delete them if there time is expired
+            foreach ($streams as $this->stream) {
+                $schedule->call(function () {
+                    $this->disable_stream_target($this->stream->app, $this->stream->stream);
+                    $this->stream->delete();
+                    // echo "Stoped success....\n\n";
+                })->when(function () {
+                    return \Carbon\Carbon::createFromFormat('m/d/Y H:i', $this->stream->start_time->format('m/d/Y H:i'))->isPast() && \Carbon\Carbon::createFromFormat('m/d/Y H:i', $this->stream->end_time->format('m/d/Y H:i'))->isPast();
+                });
+            }
         }
     }
 
@@ -65,19 +60,41 @@ class Kernel extends ConsoleKernel
 
     public function enable_this_stream($app, $name)
     {
-        $response =  Http::accept('application/json')->withHeaders([
+        $reqest = Http::accept('application/json')->withHeaders([
             "Accept:application/json; charset=utf-8",
-            'Content-Type:application/json; charset=utf-8',
-        ])->put(env("WOWZA_HOST_FULL_API_URL") . '/v2/servers/_defaultServer_/vhosts/_defaultVHost_/applications/' . $app . '/pushpublish/mapentries/' . $name . '/actions/enable');
-        return $response;
+        ])->get(env("WOWZA_HOST_FULL_API_URL") . '/v2/servers/_defaultServer_/vhosts/_defaultVHost_/applications/' . $app . '/pushpublish/mapentries')->collect()['mapEntries'];
+
+        // check if the stream target is already enabled
+        if (!$reqest[0]['enabled']) {
+            try {
+                Http::accept('application/json')->withHeaders([
+                    "Accept:application/json; charset=utf-8",
+                    'Content-Type:application/json; charset=utf-8',
+                ])->put(env("WOWZA_HOST_FULL_API_URL") . '/v2/servers/_defaultServer_/vhosts/_defaultVHost_/applications/' . $app . '/pushpublish/mapentries/' . $name . '/actions/enable');
+            } catch (\Throwable $th) {
+                echo $th;
+            }
+        }
+        echo "starting success....\n\n";
     }
 
     public function disable_stream_target($app, $name)
     {
-        $response =  Http::accept('application/json')->withHeaders([
+        $reqest = Http::accept('application/json')->withHeaders([
             "Accept:application/json; charset=utf-8",
-            'Content-Type:application/json; charset=utf-8',
-        ])->put(env("WOWZA_HOST_FULL_API_URL") . '/v2/servers/_defaultServer_/vhosts/_defaultVHost_/applications/' . $app . '/pushpublish/mapentries/' . $name . '/actions/disable');
-        return $response;
+        ])->get(env("WOWZA_HOST_FULL_API_URL") . '/v2/servers/_defaultServer_/vhosts/_defaultVHost_/applications/' . $app . '/pushpublish/mapentries')->collect()['mapEntries'];
+
+        // check if the stream target is already disabled
+        if ($reqest[0]['enabled']) {
+            try {
+                Http::accept('application/json')->withHeaders([
+                    "Accept:application/json; charset=utf-8",
+                    'Content-Type:application/json; charset=utf-8',
+                ])->put(env("WOWZA_HOST_FULL_API_URL") . '/v2/servers/_defaultServer_/vhosts/_defaultVHost_/applications/' . $app . '/pushpublish/mapentries/' . $name . '/actions/disable');
+            } catch (\Throwable $th) {
+                echo $th;
+            }
+        }
+        echo "Stoped success....\n\n";
     }
 }
